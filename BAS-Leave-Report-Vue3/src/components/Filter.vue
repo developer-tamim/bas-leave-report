f2a68dda7c30e9c459b25cd02c67883e1e514b76
developer-tamim <tawsiftamim24@gmail.com>
2024-05-16 15:44:50 +0600

    Filter Component

---
+++ BAS-Leave-Report-Vue3/src/components/Filter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="card-body">
+      <h4>Filters</h4>
+      <div class="form-group mb-3">
+        <label for="attend">Attend</label><br />
+        <select id="attend" class="form-control">
+          <option value="All">All</option>
+          <option value="Mohammed Kaium Uddin">Mohammed Kaium Uddin</option>
+          <option value="Abu Zahin">Abu Zahin</option>
+        </select>
+      </div>
+      <div class="form-group mb-3">
+        <label for="from">From</label><br />
+        <input type="date" id="from" value="2024-01-01" class="form-control" />
+      </div>
+      <div class="form-group mb-3">
+        <label for="to">To</label><br />
+        <input type="date" id="to" value="2024-01-30" class="form-control" />
+      </div>
+      <div class="form-group mb-3">
+        <label for="team">Team</label><br />
+        <select id="teamFilter" class="form-control">
+          <option value="All">All</option>
+          <option value="Team 1">Team 1</option>
+          <option value="Team 2">Team 2</option>
+        </select>
+      </div>
+      <div class="form-group mb-3">
+        <button class="btn btn-danger">Reset</button>
+        <button class="btn btn-export">Export</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filter",
+};
+</script>
--- BAS-Leave-Report-Vue3/src/components/LeaveReport.vue
+++ BAS-Leave-Report-Vue3/src/components/LeaveReport.vue
@@ -5,40 +5,15 @@
         <div class="col-lg-2 col-md-4 order-md-2">
           <div class="sticky-top">
             <div class="card p-3" id="filterSection">
-              <div class="card-body">
-                <h4>Filters</h4>
-                <div class="form-group mb-3">
-                  <label for="attend">Attend</label><br />
-                  <select id="attend" class="form-control">
-                    <option value="All">All</option>
-                    <option value="Mohammed Kaium Uddin">Mohammed Kaium Uddin</option>
-                    <option value="Abu Zahin">Abu Zahin</option>
-                  </select>
-                </div>
-                <div class="form-group mb-3">
-                  <label for="from">From</label><br />
-                  <input type="date" id="from" value="2024-01-01" class="form-control" />
-                </div>
-                <div class="form-group mb-3">
-                  <label for="to">To</label><br />
-                  <input type="date" id="to" value="2024-01-30" class="form-control" />
-                </div>
-                <div class="form-group mb-3">
-                  <label for="team">Team</label><br />
-                  <select id="teamFilter" class="form-control">
-                    <option value="All">All</option>
-                    <option value="Team 1">Team 1</option>
-                    <option value="Team 2">Team 2</option>
-                  </select>
-                </div>
-                <div class="form-group mb-3">
-                  <button class="btn btn-danger">Reset</button>
-                  <button class="btn btn-export">Export</button>
-                </div>
-              </div>
+
+              <!-- Filter -->
+              <Filter/>
+              <!-- Filter -->
+
             </div>
           </div>
         </div>
+       
   
         <!-- Main content -->
         <div class="col-lg-10 col-md-8 order-md-1">
@@ -70,14 +45,13 @@
   
   <script>
   import Table from './Table.vue'
+import Filter from './Filter.vue'
   export default {
     name: 'LeaveReport',
-	components: { Table },
+	components: { Table, Filter },
    
   };
   </script>
   
-  <style scoped>
-  /* Add your styles here */
-  </style>
+
   
