a0faf1d66243936e26c92c118d68cb9ed93584ed
developer-tamim <tawsiftamim24@gmail.com>
2024-05-17 14:53:28 +0600

    modal create

--- BAS-Leave-Report-Vue3/src/components/Table.vue
+++ BAS-Leave-Report-Vue3/src/components/Table.vue
@@ -17,7 +17,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(row, index) in rows" :key="index">
+          <!-- <tr v-for="(row, index) in rows" :key="index">
             <td>{{ row.id }}</td>
             <td>{{ row.name }}</td>
             <td>{{ row.designation }}</td>
@@ -27,7 +27,53 @@
             <td>{{ row.late }}</td>
             <td>{{ row.yearsOfService }}</td>
             <td>{{ row.joiningDate }}</td>
+          </tr> -->
+        
+          <tr>
+            <td>ISL 12025</td>
+            <td>Mohammed Shumon</td>
+            <td>HR</td>
+            <td>Appraisal</td>
+            <td>5 day</td>
+            <td>3 day</td>
+            <td>1 day</td>
+            <td>14 years</td>
+            <td>02/26/2008</td>
           </tr>
+          <tr>
+            <td>ISL 12025</td>
+            <td>Mohammed Shumon</td>
+            <td>HR</td>
+            <td>Appraisal</td>
+            <td>5 day</td>
+            <td>3 day</td>
+            <td>1 day</td>
+            <td>14 years</td>
+            <td>02/26/2008</td>
+          </tr>
+          <tr>
+            <td>ISL 12025</td>
+            <td>Mohammed Shumon</td>
+            <td>HR</td>
+            <td>Appraisal</td>
+            <td>5 day</td>
+            <td>3 day</td>
+            <td>1 day</td>
+            <td>14 years</td>
+            <td>02/26/2008</td>
+          </tr>
+          <tr>
+            <td>ISL 12025</td>
+            <td>Mohammed Shumon</td>
+            <td>HR</td>
+            <td>Appraisal</td>
+            <td>5 day</td>
+            <td>3 day</td>
+            <td>1 day</td>
+            <td>14 years</td>
+            <td>02/26/2008</td>
+          </tr>
+         
         </tbody>
       </table>
     </div>
@@ -48,70 +94,70 @@
 <script>
 export default {
   name: "Table",
-  data() {
-    return {
-      rows: [],
-    };
-  },
-  mounted() {
-    this.addRandomRows(30);
-  },
-  methods: {
-    generateRandomRow() {
-      const ids = [
-        "ISL 12025",
-        "ISL 12026",
-        "ISL 12027",
-        "ISL 12028",
-        "ISL 12029",
-      ];
-      const names = ["Mohammed Kaium Uddin", "Abu Zahin"];
-      const designations = [
-        "Co-Originator",
-        "Developer",
-        "Designer",
-        "Manager",
-        "Analyst",
-      ];
-      const teams = ["Software", "Marketing", "Sales", "Finance", "Operations"];
-      const leaves = ["1 Day", "2 Days", "3 Days", "4 Days", "5 Days"];
-      const homeOffices = ["-", "1 Day", "2 Days", "3 Days", "4 Days"];
-      const lates = ["Late", "On Time"];
-      const yearsOfService = [
-        "5 years",
-        "6 years",
-        "7 years",
-        "8 years",
-        "9 years",
-      ];
-      const joiningDates = [
-        "01/25/2012",
-        "03/14/2015",
-        "07/02/2017",
-        "09/10/2019",
-        "12/30/2021",
-      ];
+  // data() {
+  //   return {
+  //     rows: [],
+  //   };
+  // },
+  // mounted() {
+  //   this.addRandomRows(30);
+  // },
+  // methods: {
+  //   generateRandomRow() {
+  //     const ids = [
+  //       "ISL 12025",
+  //       "ISL 12026",
+  //       "ISL 12027",
+  //       "ISL 12028",
+  //       "ISL 12029",
+  //     ];
+  //     const names = ["Mohammed Kaium Uddin", "Abu Zahin"];
+  //     const designations = [
+  //       "Co-Originator",
+  //       "Developer",
+  //       "Designer",
+  //       "Manager",
+  //       "Analyst",
+  //     ];
+  //     const teams = ["Software", "Marketing", "Sales", "Finance", "Operations"];
+  //     const leaves = ["1 Day", "2 Days", "3 Days", "4 Days", "5 Days"];
+  //     const homeOffices = ["-", "1 Day", "2 Days", "3 Days", "4 Days"];
+  //     const lates = ["Late", "On Time"];
+  //     const yearsOfService = [
+  //       "5 years",
+  //       "6 years",
+  //       "7 years",
+  //       "8 years",
+  //       "9 years",
+  //     ];
+  //     const joiningDates = [
+  //       "01/25/2012",
+  //       "03/14/2015",
+  //       "07/02/2017",
+  //       "09/10/2019",
+  //       "12/30/2021",
+  //     ];
 
-      return {
-        id: ids[Math.floor(Math.random() * ids.length)],
-        name: names[Math.floor(Math.random() * names.length)],
-        designation:
-          designations[Math.floor(Math.random() * designations.length)],
-        team: teams[Math.floor(Math.random() * teams.length)],
-        leave: leaves[Math.floor(Math.random() * leaves.length)],
-        homeOffice: homeOffices[Math.floor(Math.random() * homeOffices.length)],
-        late: lates[Math.floor(Math.random() * lates.length)],
-        yearsOfService:
-          yearsOfService[Math.floor(Math.random() * yearsOfService.length)],
-        joiningDate:
-          joiningDates[Math.floor(Math.random() * joiningDates.length)],
-      };
-    },
-    addRandomRows(numRows) {
-      for (let i = 0; i < numRows; i++) {
-        this.rows.push(this.generateRandomRow());
-      }
-    },
-  },
+  //     return {
+  //       id: ids[Math.floor(Math.random() * ids.length)],
+  //       name: names[Math.floor(Math.random() * names.length)],
+  //       designation:
+  //         designations[Math.floor(Math.random() * designations.length)],
+  //       team: teams[Math.floor(Math.random() * teams.length)],
+  //       leave: leaves[Math.floor(Math.random() * leaves.length)],
+  //       homeOffice: homeOffices[Math.floor(Math.random() * homeOffices.length)],
+  //       late: lates[Math.floor(Math.random() * lates.length)],
+  //       yearsOfService:
+  //         yearsOfService[Math.floor(Math.random() * yearsOfService.length)],
+  //       joiningDate:
+  //         joiningDates[Math.floor(Math.random() * joiningDates.length)],
+  //     };
+  //   },
+  //   addRandomRows(numRows) {
+  //     for (let i = 0; i < numRows; i++) {
+  //       this.rows.push(this.generateRandomRow());
+  //     }
+  //   },
+  // },
 };
 </script>
--- BAS-Leave-Report-Vue3/src/components/Test.vue
+++ BAS-Leave-Report-Vue3/src/components/Test.vue
@@ -1,10 +1,184 @@
 <template>
     <div>
-      <div class="card m-3 ml-auto" style="width:50%">
-        <div class="card-body">
-            test
+      <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">See details</button>
+
+      <table class="table">
+        <thead>
+          <tr data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            <th>Date</th>
+            <th>Leave Type</th>
+            <th>Status</th>
+            <th>Approved/Rejected By</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>02/26/2024</td>
+            <td>Sick Leave</td>
+            <td>Approved</td>
+            <td>Mohammed Shumon</td>
+          </tr>
+          <tr>
+            <td>02/26/2024</td>
+            <td>Sick Leave</td>
+            <td>Approved</td>
+            <td>Shimul Barua</td>
+          </tr>
+          <tr>
+            <td>02/26/2024</td>
+            <td>Married Leave</td>
+            <td>Approved</td>
+            <td>Mohammed Kaium Uddin</td>
+          </tr>
+          
+        </tbody>
+      </table>
+
+<div class="offcanvas offcanvas-end" style="width:50%" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div class="card m-3" style="border:none">
+      <div class="card-body">
+          
+        <!-- employee details -->
+        <section>
+          <div class="row">
+            <div class="col-4">
+              <div>
+                <div class="rounded-container">
+                    <img src="../assets/default_profile.png" class="rounded-circle rounded-image" alt="Profile Image">
+                </div>
+            </div>
+            </div>
+            <div class="col-8">
+              <h5 class="bold">Mohammad Kaium Uddin</h5>
+              <h5>CoOrdinator <span>(Software & Accounting Team)</span></h5>
+              <span>ID: ISL 12025</span><br>
+              <span>Chittagong, BD</span>
+            </div>
+          </div>
+        </section>
+        <!-- employee details -->
+
+        <!-- employee sub details -->
+        <section>
+          <div class="row m-2">
+            <div class="col-4">
+              <div class="row my-3" >
+                <div class="col-3">
+                  <i class="fa-regular fa-clock" style="font-size: 25px;"></i>
+                </div>
+                <div class="col-9">
+                  <span>01/05/2015</span><br>
+                  <span>8 years</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="row my-3">
+                <div class="col-3">
+                  <i class="fa-solid fa-phone-volume" style="font-size: 25px;"></i>
+                </div>
+                <div class="col-9">
+                  <span>+880 1234 456789</span><br>
+                  <span>kaium@gmail.com</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="row my-3">
+                <div class="col-3">
+                  <i class="fa-solid fa-house" style="font-size: 25px;"></i>
+                </div>
+                <div class="col-9">
+                  <span>Chittagong Branch</span><br>
+                  <span>Day shift</span>
+                </div>
+              </div>
+            </div>
+           
+          </div>
+        </section>
+        <!-- employee sub details -->
+
+        <!-- tabs & date -->
+        <section>
+          <div class="row m-2">
+           
+            <div class="col-5">
+              <div class="row">
+                <div class="col-4">
+                  <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" autocomplete="off" checked>
+<label class="btn btn-outline-success" for="success-outlined">Leave</label>
+                </div>
+
+                <!-- <div class="col-4">
+                  <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" autocomplete="on" checked>
+<label class="btn btn-outline-success" for="success-outlined">Home</label>
+                </div> -->
+
+                <div class="col-4">
+                  <input type="radio" class="btn-check" name="options-outlined" id="danger-outlined" autocomplete="off">
+<label class="btn" for="success-outlined">Late</label>
+                </div>
+
+              </div>
+            </div>
+            <div class="col-2"></div>
+            <div class="col-5">
+              <div class="row">
+                <div class="col-4 btn btn-danger">test</div>
+                <div class="col-4 btn btn-danger">test</div>
+                <div class="col-4 btn btn-danger">test</div>
+              </div>
+            </div>
+           
+          </div>
+        </section>
+        <!-- tabs & date -->
+        
+        <!-- table -->
+        <div class="mt-4">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Leave Type</th>
+                <th>Status</th>
+                <th>Approved/Rejected By</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>02/26/2024</td>
+                <td>Sick Leave</td>
+                <td>Approved</td>
+                <td>Mohammed Shumon</td>
+              </tr>
+              <tr>
+                <td>02/26/2024</td>
+                <td>Sick Leave</td>
+                <td>Approved</td>
+                <td>Shimul Barua</td>
+              </tr>
+              <tr>
+                <td>02/26/2024</td>
+                <td>Married Leave</td>
+                <td>Approved</td>
+                <td>Mohammed Kaium Uddin</td>
+              </tr>
+              
+            </tbody>
+          </table>
         </div>
+        <!-- table -->
       </div>
+    </div>
+  </div>
+</div>
     </div>
   </template>
   
