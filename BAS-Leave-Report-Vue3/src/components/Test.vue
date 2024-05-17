25a34bd9ca058608233ee7d9eebd8b353efd7f91
developer-tamim <tawsiftamim24@gmail.com>
2024-05-17 15:32:00 +0600

    ..

--- BAS-Leave-Report-Vue3/src/components/Test.vue
+++ BAS-Leave-Report-Vue3/src/components/Test.vue
@@ -109,14 +109,19 @@
           <div class="row m-2">
            
             <div class="col-5">
-              <div class="btn-group" role="group" aria-label="Basic outlined example">
-                <button type="button" class="btn btn-outline-primary">Leave 4</button>
-                <button type="button" class="btn btn-outline-primary">Home 7</button>
-                <button type="button" class="btn btn-outline-primary">Late 2</button>
+              <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                <label class="btn btn-outline-primary" for="btnradio1">Leave 14</label>
+              
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio2">Home 5</label>
+              
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio3">Late 2</label>
               </div>
             </div>
             <div class="col-2"></div>
-            <div class="col-5">
+            <!-- <div class="col-5">
               <div class="btn-group" role="group" aria-label="Basic outlined example">
                 <button type="button" class="btn">Start to End Date</button>
                 <button type="button" class="btn btn-outline">
@@ -126,8 +131,8 @@
                   <input type="date" style="border:none;">
                 </button>
               </div>
-            </div>
-            <!-- <div class="col-5">
+            </div> -->
+            <div class="col-5">
               <div class="row">
                 <div class="col-4">
                   <span>Start to End Date</span>
@@ -139,7 +144,7 @@
                   <input type="date">
                 </div>
               </div>
-            </div> -->
+            </div>
            
           </div>
         </section>
